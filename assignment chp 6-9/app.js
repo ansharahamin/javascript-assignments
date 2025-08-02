bb5711c9ba7caee3084127ac52730a63f7ebbf4a
ansharahamin <insharahamin1250@gmail.com>
2025-08-02 23:09:19 +0500

    Qno1 done

---
+++ assignment chp 6-9/app.js
@@ -0,0 +1,15 @@
+// -------------------chp#6---------------------
+// ---Qno1---
+var a=10
+document.write("<h1> Result : </h1>" + "<br>"+ "the value of num is : " + a +"<br>" + "---------------------------- <br>")
+
+document.write("the value of ++a is : "+  ++a +"<br>")
+document.write(" now the value of a is : "+  a +"<br><br>")
+
+document.write("the value of a++ is : "+  a++ +"<br>")
+document.write("now, the value of a is : "+  a +"<br> <br>")
+document.write("the value of --a is : " + --a +"<br>")
+document.write(" now the value of a is : "+  a +"<br><br>")
+document.write("the value of a-- is : " + a-- + "<br>")
+document.write(" now the value of a is : "+  a +"<br><br>")
+
