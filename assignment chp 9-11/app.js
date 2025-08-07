2710a0e332a3f7b464910fee0a11b05599e4c5f5
ansharahamin <insharahamin1250@gmail.com>
2025-08-07 23:07:12 +0500

    qn04

---
+++ assignment chp 9-11/app.js
@@ -0,0 +1,39 @@
+// Qno1
+
+// var userCity = prompt("Enter your city");
+// if(userCity.toLowerCase() === "karachi") {
+//     console.log("Welcome to the city of lights!!");
+    
+// }
+// Qno2
+
+// var gender = prompt("Enter your Gender");
+// if (gender.toLowerCase() === "male"){
+//     console.log("Good Morning Sir!!");
+// }else if (gender.toLowerCase() === "female"){
+//     console.log("Good Morning Ma'am!!");
+// }else{
+    // console.log("Invalid input");}
+
+// Qno3
+
+// var color = prompt("Enter traffic light color")
+// if(color.toLowerCase() === "red"){
+//     console.log("must Stop!!");
+// }else if (color.toLowerCase() === "yellow"){
+//     console.log("Ready to  move!!");
+// }else if (color.toLowerCase() === "green"){
+//     console.log(("Move Now!!"));
+    
+// }else{
+//     console.log("enter a valid color");
+    
+// }
+
+// Qno4
+
+var  fuel = +prompt("Enter remaining fuel in litres");
+if (fuel < 0.25){
+
+    console.log("Please refill the fuel in your car");
+}
