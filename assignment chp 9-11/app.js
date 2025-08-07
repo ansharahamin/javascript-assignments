f0dd6727778dffa9dc266422434eb273828d12ae
ansharahamin <insharahamin1250@gmail.com>
2025-08-08 01:03:16 +0500

    q:5 done

--- assignment chp 9-11/app.js
+++ assignment chp 9-11/app.js
@@ -32,8 +32,64 @@
 
 // Qno4
 
-var  fuel = +prompt("Enter remaining fuel in litres");
-if (fuel < 0.25){
+// var  fuel = +prompt("Enter remaining fuel in litres");
+// if (fuel < 0.25){
 
-    console.log("Please refill the fuel in your car");
-}
+//     console.log("Please refill the fuel in your car");
+// }
+
+// Qno5
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+
+
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+
+// output
+
+//  given condition for variable a is true
+
+//  condition 2 is true
+
+//  condition 4 is true
+
+//  The cost equals
+
+//  True
+
+//  car is smaller than cat
