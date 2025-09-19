6456fc4a77b0a4d62de263c1d61e78a9b38b5fa6
ansharahamin <insharahamin1250@gmail.com>
2025-09-19 17:59:33 +0500

    new work

--- assignment chp26-30/app.js
+++ assignment chp26-30/app.js
@@ -1,4 +1,4 @@
-
+// ================  Replace task==========
 // var str = "Kashmir (/ˈkæʃmɪər/ KASH-meer or /kæʃˈmɪər/ kash-MEER) is the northernmost geographical region of the Indian subcontinent. Until the mid-19th century, the term Kashmir denoted only the Kashmir Valley between the Great Himalayas and the Pir Panjal Range. The term has since also come to encompass a larger area that formerly comprised the princely state of Jammu and Kashmir, and includes the Indian-administered territories of Jammu and Kashmir and Ladakh, the Pakistani-administered territories of Azad Kashmir and Gilgit-Baltistan, and the Chinese-administered territories of Aksai Chin and the Trans-Karakoram Tract.[1][2][3]"
 // var update = str.replace(/Kashmir/g,"pakistan")
 // console.log(update);
@@ -15,6 +15,8 @@ var str = "Kashmir (/ˈkæʃmɪər/ KASH-meer or /kæʃˈmɪər/ kash-MEER) is t
 // var fchr =str.indexOf("Kashmir")
 // var result = str.slice(0,fchr)+"pakistan " + str.slice(fchr+7)
 
+// ============Qno1======================
+
 // var num = prompt("  Enter a positive integer")||3.45214
 // if(num>=0){
 //     var round =Math.round(num)
@@ -24,7 +26,7 @@ var str = "Kashmir (/ˈkæʃmɪər/ KASH-meer or /kæʃˈmɪər/ kash-MEER) is t
 // }else{
 //     alert("Enter a positive number")
 // }
-
+// ===================Qno2===================
 // var num = prompt("  Enter a negative floating number")||-2.6738 
 // if(num<=0 && num.includes(".")){
 //     var round =Math.round(num)
@@ -34,6 +36,8 @@ var str = "Kashmir (/ˈkæʃmɪər/ KASH-meer or /kæʃˈmɪər/ kash-MEER) is t
 // }else{
 //     alert("Enter a  negative floating number")
 // }
+ 
+// ============Qno3==================
 
 // var num = prompt("Enter a number to find its absolute value")||-4
 // if(num<0){
@@ -43,7 +47,9 @@ var str = "Kashmir (/ˈkæʃmɪər/ KASH-meer or /kæʃˈmɪər/ kash-MEER) is t
 
 //     document.write("The absolute value of " + num + " is " + abs)
 // }
- 
+   
+// =================Qno4=================
+
 // for (let i = 0; i < 6; i++) {
 //     var dice = Math.ceil(Math.random()*6)
 //   document.write("random dice value:  "+ dice +"<br>") ;
@@ -52,6 +58,8 @@ var str = "Kashmir (/ˈkæʃmɪər/ KASH-meer or /kæʃˈmɪər/ kash-MEER) is t
 //   document.write("You rolled: " + diceFaces[dice - 1] + "<br>");
 
 // }
+
+// ==========password generation=============
 
 // var keyboardChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/? ";
 // var password = ""
@@ -67,11 +75,47 @@ var str = "Kashmir (/ˈkæʃmɪər/ KASH-meer or /kæʃˈmɪər/ kash-MEER) is t
 // }
 // console.log(password);
 
-// date
+//========= date ==============
 
 // var date = new Date()
 // day = date.getDay()
 // arr = ["sun","Mon","tue" , "wed" ,"thur" ,"fri","sat"]
 // console.log(arr[day]);
 
+// ===========Qno5============\\
+ 
 
+//     var toss = Math.ceil(Math.random()*2)
+//     if(toss===1){
+         
+//     }
+//    document.write(toss);
+
+//============== Age Calculator ========
+
+// var userDob = prompt("Enter your date of birth: " ,"28-february-2007")
+// var dob = new Date(userDob) 
+// var now = dob.getTime();
+// var today = new Date()
+// var mili = today.getTime();
+// var diff = mili - now 
+// var age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+// console.log("Your age is: " + age + " years");
+
+// ========Ramzan Counter==========
+// var ramzan = new Date("17-february-2026") 
+// var now = ramzan.getMonth();
+// var today = new Date();
+// var mili = today.getMonth();
+// var diff = mili - now 
+// var month = Math.floor(diff / (1000 * 60 * 60 * 24 *30));
+// console.log("Ramzan will come after : " + month + " month");
+
+
+
+
+// var ramzan = new Date("17-february-2026")
+// var now = new Date()
+// var dif = ramzan.getTime()-now.getTime()
+// var conversion = Math.round(dif/(1000*60*60*24))
+// console.log(conversion);
