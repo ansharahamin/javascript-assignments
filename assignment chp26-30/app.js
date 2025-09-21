03789af5fa063427a81d266907a35f753bca7a25
ansharahamin <insharahamin1250@gmail.com>
2025-09-22 01:19:38 +0500

    assignment is done

--- assignment chp26-30/app.js
+++ assignment chp26-30/app.js
@@ -1,19 +1,4 @@
-// ================  Replace task==========
-// var str = "Kashmir (/ˈkæʃmɪər/ KASH-meer or /kæʃˈmɪər/ kash-MEER) is the northernmost geographical region of the Indian subcontinent. Until the mid-19th century, the term Kashmir denoted only the Kashmir Valley between the Great Himalayas and the Pir Panjal Range. The term has since also come to encompass a larger area that formerly comprised the princely state of Jammu and Kashmir, and includes the Indian-administered territories of Jammu and Kashmir and Ladakh, the Pakistani-administered territories of Azad Kashmir and Gilgit-Baltistan, and the Chinese-administered territories of Aksai Chin and the Trans-Karakoram Tract.[1][2][3]"
-// var update = str.replace(/Kashmir/g,"pakistan")
-// console.log(update);
 
-var str = "Kashmir (/ˈkæʃmɪər/ KASH-meer or /kæʃˈmɪər/ kash-MEER) is the northernmost geographical region of the Indian subcontinent. Until the mid-19th century, the term Kashmir denoted only the Kashmir Valley between the Great Himalayas and the Pir Panjal Range. The term has since also come to encompass a larger area that formerly comprised the princely state of Jammu and Kashmir, and includes the Indian-administered territories of Jammu and Kashmir and Ladakh, the Pakistani-administered territories of Azad Kashmir and Gilgit-Baltistan, and the Chinese-administered territories of Aksai Chin and the Trans-Karakoram Tract.[1][2][3]"
-
-// for (var i = 0; i < str.length; i++) {//1
-//    if(str.slice(i,i+7)==="Kashmir"){
-//     str =str.slice(0,i)+ "pakistan " + str .slice(i+7)
-//    }
-    
-// }
-// console.log(str);
-// var fchr =str.indexOf("Kashmir")
-// var result = str.slice(0,fchr)+"pakistan " + str.slice(fchr+7)
 
 // ============Qno1======================
 
@@ -59,6 +44,75 @@ var str = "Kashmir (/ˈkæʃmɪər/ KASH-meer or /kæʃˈmɪər/ kash-MEER) is t
 
 // }
 
+// ===========Qno5============\\
+ 
+
+    // var toss = Math.ceil(Math.random()*2)
+    // var result;
+    // if(toss===1){
+    //     result = "Heads"
+    //      document.write(toss+"<br>"+"Random coin value: "+ result  )
+    // }else{
+    //     result="Tails"
+    //              document.write(toss+"<br>"+"Random coin value: "+ result  )
+
+    // }
+
+// ===========Qno6============\\
+
+// var num = Math.ceil(Math.random()*100)
+// document.write("The Random number Between 1 and 100 is: "+num);
+
+
+// ===========Qno7============\\
+
+// 7. Write a program that asks the user about his weight. Parse
+// the user input and display his weight in your browser.
+// Possible user inputs can be:
+// a. 50
+// b. 50kgs
+// c. 50.2kgs
+// d. 50.2kilograms
+
+// var weight = prompt("Enter Your Weight in kilograms")
+// result = parseFloat(weight)
+// document.write("The Weight of user is: " + result + " kilograms")
+
+
+// // ===========Qno8============\\
+
+// 8. Write a program that stores a random secret number from
+// 1 to 10 in a variable. Ask the user to input a number
+// between 1 and 10. If the user input equals the secret
+// number, congratulate the user.
+
+// var secretNum = Math.ceil(Math.random()*10)
+// console.log(secretNum);
+
+// userInput = +prompt("Enter a number between 1 and 10 ")
+// if(userInput>=1&&userInput<=10){
+// if(userInput===secretNum){
+//     Swal.fire({
+//   title: "Good job!",
+//   text: "You guessed the number!",
+//   icon: "success"
+// });
+// }else{
+// Swal.fire({
+//   icon: "error",
+//   title: "Oops...",
+//   text: "Wrong guess! The correct number was " + secretNum,
+//   footer: '<a href="#">Why do I have this issue?</a>'
+// });
+// }
+// }else{
+//         Swal.fire({
+//       icon: "warning",
+//       title: "Invalid Input",
+//       text: "Please enter a number between 1 and 10 only."
+//     });
+// }
+
 // ==========password generation=============
 
 // var keyboardChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/? ";
@@ -82,14 +136,8 @@ var str = "Kashmir (/ˈkæʃmɪər/ KASH-meer or /kæʃˈmɪər/ kash-MEER) is t
 // arr = ["sun","Mon","tue" , "wed" ,"thur" ,"fri","sat"]
 // console.log(arr[day]);
 
-// ===========Qno5============\\
- 
 
-//     var toss = Math.ceil(Math.random()*2)
-//     if(toss===1){
-         
-//     }
-//    document.write(toss);
+
 
 //============== Age Calculator ========
 
@@ -119,3 +167,21 @@ var str = "Kashmir (/ˈkæʃmɪər/ KASH-meer or /kæʃˈmɪər/ kash-MEER) is t
 // var dif = ramzan.getTime()-now.getTime()
 // var conversion = Math.round(dif/(1000*60*60*24))
 // console.log(conversion);
+
+
+// ================  Replace task==========
+// var str = "Kashmir (/ˈkæʃmɪər/ KASH-meer or /kæʃˈmɪər/ kash-MEER) is the northernmost geographical region of the Indian subcontinent. Until the mid-19th century, the term Kashmir denoted only the Kashmir Valley between the Great Himalayas and the Pir Panjal Range. The term has since also come to encompass a larger area that formerly comprised the princely state of Jammu and Kashmir, and includes the Indian-administered territories of Jammu and Kashmir and Ladakh, the Pakistani-administered territories of Azad Kashmir and Gilgit-Baltistan, and the Chinese-administered territories of Aksai Chin and the Trans-Karakoram Tract.[1][2][3]"
+// var update = str.replace(/Kashmir/g,"pakistan")
+// console.log(update);
+
+// var str = "Kashmir (/ˈkæʃmɪər/ KASH-meer or /kæʃˈmɪər/ kash-MEER) is the northernmost geographical region of the Indian subcontinent. Until the mid-19th century, the term Kashmir denoted only the Kashmir Valley between the Great Himalayas and the Pir Panjal Range. The term has since also come to encompass a larger area that formerly comprised the princely state of Jammu and Kashmir, and includes the Indian-administered territories of Jammu and Kashmir and Ladakh, the Pakistani-administered territories of Azad Kashmir and Gilgit-Baltistan, and the Chinese-administered territories of Aksai Chin and the Trans-Karakoram Tract.[1][2][3]"
+
+// for (var i = 0; i < str.length; i++) {//1
+//    if(str.slice(i,i+7)==="Kashmir"){
+//     str =str.slice(0,i)+ "pakistan " + str .slice(i+7)
+//    }
+    
+// }
+// console.log(str);
+// var fchr =str.indexOf("Kashmir")
+// var result = str.slice(0,fchr)+"pakistan " + str.slice(fchr+7)
