64e1a56b4cbd752025b737453dc8e417e5bc7f89
ansharahamin <insharahamin1250@gmail.com>
2025-09-26 15:32:55 +0500

    Qno3

---
+++ assignment chp 35-38/app.js
@@ -0,0 +1,90 @@
+//============ Qno01 ===============\\
+
+// function dateAndTime(currentTime) {
+//     console.log(currentTime);
+    
+// }
+// now = new Date()
+// dateAndTime(now
+
+
+// )
+//============ Qno02 ===============\\
+
+// function userName(fName,LName) {
+//     console.log( "Welcome "+fName+" "+LName+ " !!");
+    
+// }
+// var firstname = prompt("Enter you first name")
+// var lastname = prompt("Enter you last name")
+// userName(firstname,lastname)
+
+//============ Qno03 ===============\\
+
+// function sum(num1,num2) {
+//     console.log(num1+num2);
+    
+// }
+// var userValue1 = Number(prompt("Enter first number"))
+// var userValue2 = Number(prompt("Enter second number"))
+// sum(userValue1,userValue2)
+
+//============ Qno04 ===============\\
+
+// 4. Calculator:
+// Write a function that takes three arguments num1, num2
+// & operator & compute the desired operation. Return and
+// show the desired result in your browser.
+
+// function calculator(num1,num2,operator) {
+//    let  result;
+//     if(operator==="+"){
+//         result = num1+num2
+//     }else if(operator==="-"){
+//         result = num1 - num2
+//     }else if(operator==="*"){
+//         result = num1 * num2
+//     }else if(operator==="/"){
+//         result = num1 / num2
+//     }else if(operator==="%"){
+//         result = num1 % num2
+//     }else{
+//         result= "invalid operator"
+//     }
+//     return result
+// }
+// var num1=+prompt("Enter first number")
+// var num2=+prompt("Enter second number")
+// var operator=prompt("Enter operator")
+
+// alert(calculator(num1,num2,operator))
+
+//============ Qno05 ===============\\
+
+// function square(num1) {
+//     let result = num1*num1
+//     return result;
+// }
+// let usernum = +prompt("enter a number")
+// console.log(square(usernum));
+
+
+//============ Qno06 ===============\\
+
+function factorial(num1) {
+    for (let i = 0; i < 
+        h; i++) {
+        const element = array[i];
+        
+    }
+}
+
+//============ Qno07 ===============\\
+
+
+
+//============ Qno01 ===============\\
+//============ Qno01 ===============\\
+//============ Qno01 ===============\\
+//============ Qno01 ===============\\
+//============ Qno01 ===============\\
