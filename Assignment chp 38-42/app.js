52c7414e0c72cae90d825be1f3afc79b32466541
ansharahamin <insharahamin1250@gmail.com>
2025-10-10 18:01:19 +0500

    Qno4

--- Assignment chp 38-42/app.js
+++ Assignment chp 38-42/app.js
@@ -24,15 +24,39 @@
 // LeapYear(year);
 
 //  Qno 3 
-function calcArea(a,b,c) {
-    function calcSemiParams() {
-    return (a+b +c )/2
- }
-let S = calcSemiParams()
-var Area =Math.sqrt((S*(S-a)*(S-b)*(S-c))) 
-return Area
+// function calcArea(a,b,c) {
+//     function calcSemiParams() {
+//     return (a+b +c )/2
+//  }
+// let S = calcSemiParams()
+// var Area =Math.sqrt((S*(S-a)*(S-b)*(S-c))) 
+// return Area
+// }
+// userSideA = +prompt("Enter first side")
+// userSideB = +prompt("Enter Second side")
+// userSideC = +prompt("Enter third side")
+// console.log(calcArea(userSideA,userSideB,userSideC));
+
+
+
+
+function main(total,x,y,z) {
+    function Avg() {
+
+         var avg =(x+y+z)/3
+         return avg
+     }
+     function percentage() {
+    
+       var obt =  x+y+z
+       var percent = (obt/total)*100
+       return percent
+    }
+
+  console.log("your percentage is " + percentage() + "%");
+  console.log("your Average is " + Avg());
 }
-userSideA = +prompt("Enter first side")
-userSideB = +prompt("Enter Second side")
-userSideC = +prompt("Enter third side")
-console.log(calcArea(userSideA,userSideB,userSideC));
+var sub1 = +prompt("Enter first subject marks out of 100")
+var sub2 = +prompt("Enter Second subject marks out of 100")
+var sub3 = +prompt("Enter third subject marks out of 100")
+main(300,sub1,sub2,sub3)
