eed6c780dc293d7d25959a7c091c321353b1ce41
ansharahamin <insharahamin1250@gmail.com>
2025-10-13 01:20:31 +0500

    Qnoo5

--- Assignment chp 38-42/app.js
+++ Assignment chp 38-42/app.js
@@ -38,25 +38,68 @@
 // console.log(calcArea(userSideA,userSideB,userSideC));
 
 
+// =========Qno 04===========\\
 
+// function main(total,x,y,z) {
+//     function Avg() {
 
-function main(total,x,y,z) {
-    function Avg() {
-
-         var avg =(x+y+z)/3
-         return avg
-     }
-     function percentage() {
+//          var avg =(x+y+z)/3
+//          return avg
+//      }
+//      function percentage() {
     
-       var obt =  x+y+z
-       var percent = (obt/total)*100
-       return percent
-    }
+//        var obt =  x+y+z
+//        var percent = (obt/total)*100
+//        return percent
+//     }
 
-  console.log("your percentage is " + percentage() + "%");
-  console.log("your Average is " + Avg());
+//   console.log("your percentage is " + percentage() + "%");
+//   console.log("your Average is " + Avg());
+// }
+// var sub1 = +prompt("Enter first subject marks out of 100")
+// var sub2 = +prompt("Enter Second subject marks out of 100")
+// var sub3 = +prompt("Enter third subject marks out of 100")
+// main(300,sub1,sub2,sub3)
+
+
+// =========Qno 05===========\\
+
+// function customIndex_of(a,b) {
+
+//   result = a.split("")
+//   for (let i = 0; i < result.length; i++) {
+// if (b===result[i]) {
+// return i
+  
+// } 
+//   }
+// return -1
+// }
+
+// var userInput = prompt("Enter a word")
+// var userInput2 = prompt("Enter a letter")
+// console.log(customIndex_of(userInput,userInput2));
+
+
+// =========Qno 06===========\\
+
+function deleteVowels(sentence) {
+  var result = ""
+  for (let i = 0; i < sentence.length; i++) {
+  var  check = sentence[i].toLowerCase()
+
+    if (check!=='a'&&check!=='e'&&check!=='i'&&check!=='o'&&check!=='u') {
+      result+= sentence[i]
+    }
+  }
+  return result
 }
-var sub1 = +prompt("Enter first subject marks out of 100")
-var sub2 = +prompt("Enter Second subject marks out of 100")
-var sub3 = +prompt("Enter third subject marks out of 100")
-main(300,sub1,sub2,sub3)
+var userInput = prompt("Enter a sentence max 25 words") 
+console.log( deleteVowels(userInput));
+
+// =========Qno 04===========\\
+// =========Qno 04===========\\
+// =========Qno 04===========\\
+// =========Qno 04===========\\
+// =========Qno 04===========\\
+// =========Qno 04===========\\
