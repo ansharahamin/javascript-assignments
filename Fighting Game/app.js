eabfbefb7e4aaa362d6032a1a5ac3e1ccf4bce61
ansharahamin <insharahamin1250@gmail.com>
2025-10-22 00:41:37 +0500

    Char1 has been

--- Fighting Game/app.js
+++ Fighting Game/app.js
@@ -1,5 +1,11 @@
 var char1 = document.getElementById ="char1"
-
+var char2 = document.getElementById ="char2"
+var left = 0
 function charactarAnimation() {
-    
+console.log(event.keyCode);
+if (event.keyCode===65) {
+    left=left+10
+    char1.style.left = 10 +"px"
 }
+}
+window.onkeydown = charactarAnimation
