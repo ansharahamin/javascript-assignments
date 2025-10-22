ea8e57acee241d7ac453e0ae2eb6d7723be75d00
ansharahamin <insharahamin1250@gmail.com>
2025-10-23 01:01:00 +0500

    chr1Animation

--- Fighting Game/app.js
+++ Fighting Game/app.js
@@ -1,38 +1,40 @@
-
-// function charactarAnimation() {
-//     console.log(event.keyCode);
-//     if (event.keyCode === 65) {
-//         left = left + 10
-//         char1.style.left = left + "px"
-//         char1.src = 'images/hol-walkf.gif'
-//     }
-// }
-var char1 = document.getElementById("char1");
+var char1 = document.getElementById("char1")
 var char2 = document.getElementById("char2");
 var left = 0;
 
 function charactarAnimation(event) {
     console.log(event.keyCode);
+    if (event.keyCode === 39) {
+        left = left + 10
+        char1.style.left = left + "px"
+        char1.src = 'images/hol-walkf.gif'
+        char1.style.transform = "scaleX(1)"
+        char1.style.height = "350px"
     
-    if (event.keyCode === 65) { // A key
-        left = left + 10;
-        char1.style.position = "relative"; // یہ ضروری ہے
-        char1.style.left = left + "px";
-        char1.src = "images/hol-walkf.gif";
+    }if (event.keyCode===37) {
+        left=left-10
+        char1.style.left = left+"px"
+        char1.style.transform = "scaleX(-1)"
+char1.style.height = "350px"
+    }
+    if (event.keyCode===13) {
+               char1.src = "images/holhorse1.gif"
+    
+        setTimeout(()=>{
+               char1.src = "images/holhorse-standfull.gif"
+
+
+          
+        })
+        char1.style.transform = "scaleX(1)"
+char1.style.height = "500px"
+        char1.style.transform = "scaleX(-1)"
+
     }
 }
+
+
 
 window.onkeydown = charactarAnimation;
 
 
-// function animateCharacter() {
-//   console.log(event.keyCode);
-//   if (event.keyCode === 68) {
-//     left = left + 10;
-//     char1.style.left = left + "px";
-//     // char1.style.left= (char1.offsetLeft+10)+"px"
-//     char1.style.transform = "scaleX(1)";
-//     //  char1.style.height = "200px"
-//   }
-// }
-window.onkeydown = charactarAnimation
