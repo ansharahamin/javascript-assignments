992702300278319623f3d7fed244bbaf35d25fcd
ansharahamin <insharahamin1250@gmail.com>
2025-10-24 15:41:03 +0500

    Qno1

--- Assignments Chap 49-52/app.js
+++ Assignments Chap 49-52/app.js
@@ -1,1 +1,12 @@
+var shortText =" Lorem ipsum dolor sit, amet consectetur adipisicing elit."
 var para = document.getElementById("para");
+
+function readMore() {
+    if (condition) {
+    
+} else {
+    
+}
+}
+
+
