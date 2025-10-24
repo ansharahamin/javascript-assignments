c47a5b1399e6ec138db526fd00adf792e082967a
ansharahamin <insharahamin1250@gmail.com>
2025-10-24 18:01:59 +0500

    Qno3

--- Assignments Chap 49-52/app.js
+++ Assignments Chap 49-52/app.js
@@ -11,7 +11,51 @@ var password = document.getElementById("password").value
     
 }
 
+function deleteRow(btn){
+var row = btn.parentNode.parentNode
+function SweetAlert(confirmation) {
+    const swalWithBootstrapButtons = Swal.mixin({
+  customClass: {
+    confirmButton: "btn btn-success",
+    cancelButton: "btn btn-danger"
+  },
+  buttonsStyling: false
+});
+swalWithBootstrapButtons.fire({
+  title: "Are you sure?",
+  text: "You won't be able to revert this!",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonText: "Yes, delete it!",
+  cancelButtonText: "No, cancel!",
+  reverseButtons: true
+}).then((result) => {
+  if (result.isConfirmed) {
+    row.remove()
+    swalWithBootstrapButtons.fire({
+      title: "Deleted!",
+      text: "This Student Record has been deleted.",
+      icon: "success"
+    });
+  } else if (
+    /* Read more about handling dismissals below */
+    result.dismiss === Swal.DismissReason.cancel
+  ) {
+    swalWithBootstrapButtons.fire({
+      title: "Cancelled",
+      text: "his Student Record is Safe :)",
+      icon: "error"
+    });
+  }
+});
+}
+SweetAlert()
+}
 
+function edit(e) {
+    var edit = e.parentNode.parentNode
+    
+}
 
 // var shortText =" Lorem ipsum dolor sit, amet consectetur adipisicing elit."
 // var para = document.getElementById("para");
