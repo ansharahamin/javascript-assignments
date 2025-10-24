cdab3bdf6248cd1a67cc43cec84037efabc93405
ansharahamin <insharahamin1250@gmail.com>
2025-10-24 17:16:21 +0500

    Qno1 Done

--- Assignments Chap 49-52/app.js
+++ Assignments Chap 49-52/app.js
@@ -1,12 +1,27 @@
-var shortText =" Lorem ipsum dolor sit, amet consectetur adipisicing elit."
-var para = document.getElementById("para");
+// Qno1
 
-function readMore() {
-    if (condition) {
+function submitt(){
     
-} else {
+var FName = document.getElementById("fname").value
+var LName = document.getElementById("lname").value
+var email = document.getElementById("email").value
+var password = document.getElementById("password").value
+    console.log(FName,LName,email,password);
+    var para = document.getElementById("para").innerHTML = "First Name = " + FName + "</br>" + "Last Name = " + LName + "</br>"+"email = " + email + "</br>"+"password = " + password + "</br>"
     
 }
-}
+
+
+
+// var shortText =" Lorem ipsum dolor sit, amet consectetur adipisicing elit."
+// var para = document.getElementById("para");
+
+// function readMore() {
+//     if () {
+    
+// } else {
+    
+// }
+// }
 
 
