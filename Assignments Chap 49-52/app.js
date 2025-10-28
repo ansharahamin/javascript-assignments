206ecc83892472c2ce6d0b4f8acef45810a4ee77
ansharahamin <insharahamin1250@gmail.com>
2025-10-28 15:39:17 +0500

    not completed yet

--- Assignments Chap 49-52/app.js
+++ Assignments Chap 49-52/app.js
@@ -12,31 +12,7 @@
 // }
 // Qno2
 
-// var table = document.getElementById("table");
-// var form = document.getElementById("form");
-// var currentRow = null; // store the row directly
 
-// function edit(btn) {
-//   currentRow = btn.parentNode.parentNode; // get that <tr>
-
-//   form.style.display = "block";
-
-//   document.getElementById("fname").value = currentRow.cells[1].innerHTML;
-//   document.getElementById("lname").value = currentRow.cells[2].innerHTML;
-//   document.getElementById("class").value = currentRow.cells[3].innerHTML;
-// }
-
-// function save() {
-//   if (currentRow) {
-//     currentRow.cells[1].innerHTML = document.getElementById("fname").value;
-//     currentRow.cells[2].innerHTML = document.getElementById("lname").value;
-//     currentRow.cells[3].innerHTML = document.getElementById("class").value;
-//     form.style.display = "none"; // hide form after save
-//     currentRow = null;
-//   } else {
-//     alert("No row selected!");
-//   }
-// }
 
 var table = document.getElementById("table")
 var form = document.getElementById("form")
@@ -63,47 +39,51 @@ function save() {
   }
 
 }
+  function cancelEdit() {
+    form.style.display = "none";
+    currentRow = null;
+  }
 
-// function deleteRow(btn){
-// var row = btn.parentNode.parentNode
-// function SweetAlert(confirmation) {
-//     const swalWithBootstrapButtons = Swal.mixin({
-//   customClass: {
-//     confirmButton: "btn btn-success",
-//     cancelButton: "btn btn-danger"
-//   },
-//   buttonsStyling: false
-// });
-// swalWithBootstrapButtons.fire({
-//   title: "Are you sure?",
-//   text: "You won't be able to revert this!",
-//   icon: "warning",
-//   showCancelButton: true,
-//   confirmButtonText: "Yes, delete it!",
-//   cancelButtonText: "No, cancel!",
-//   reverseButtons: true
-// }).then((result) => {
-//   if (result.isConfirmed) {
-//     row.remove()
-//     swalWithBootstrapButtons.fire({
-//       title: "Deleted!",
-//       text: "This Student Record has been deleted.",
-//       icon: "success"
-//     });
-//   } else if (
-//     /* Read more about handling dismissals below */
-//     result.dismiss === Swal.DismissReason.cancel
-//   ) {
-//     swalWithBootstrapButtons.fire({
-//       title: "Cancelled",
-//       text: "his Student Record is Safe :)",
-//       icon: "error"
-//     });
-//   }
-// });
-// }
-// SweetAlert()
-// }
+function deleteRow(btn){
+var row = btn.parentNode.parentNode
+function SweetAlert(confirmation) {
+    const swalWithBootstrapButtons = Swal.mixin({
+  customClass: {
+    confirmButton: "btn btn-success",
+    cancelButton: "btn btn-danger"
+  },
+  buttonsStyling: false
+});
+swalWithBootstrapButtons.fire({
+  title: "Are you sure?",
+  text: "You won't be able to revert this!",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonText: "Yes, delete it!",
+  cancelButtonText: "No, cancel!",
+  reverseButtons: true
+}).then((result) => {
+  if (result.isConfirmed) {
+    row.remove()
+    swalWithBootstrapButtons.fire({
+      title: "Deleted!",
+      text: "This Student Record has been deleted.",
+      icon: "success"
+    });
+  } else if (
+    /* Read more about handling dismissals below */
+    result.dismiss === Swal.DismissReason.cancel
+  ) {
+    swalWithBootstrapButtons.fire({
+      title: "Cancelled",
+      text: "his Student Record is Safe :)",
+      icon: "error"
+    });
+  }
+});
+}
+SweetAlert()
+}
 
 // //variables
 // var table = document.getElementById("table")
