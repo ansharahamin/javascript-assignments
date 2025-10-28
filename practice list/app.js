2ff7a1df3116d3dd79f2efbdd10504f2a28020ba
ansharahamin <insharahamin1250@gmail.com>
2025-10-28 18:02:55 +0500

    new work

---
+++ practice list/app.js
@@ -0,0 +1,28 @@
+ var container = document.getElementById("container")
+
+function showPara() {
+//     var list = `    <ul>
+//         <li>
+//             karachi
+//         </li>
+//         <li>
+//             hyd
+//         </li>
+//         <li>
+//             lahore
+//         </li>
+//         <li>
+//           isl
+//         </li>
+//     </ul>
+//        <button onclick="hideList()" type="button">Hide LIst</button>
+//  `
+   
+
+var para = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur totam minima earum id quibusdam modi quam ipsam omnis perspiciatis, unde architecto quisquam necessitatibus exercitationem fuga tempore, mollitia alias quo doloremque.`
+   document.getElementById(para).innerHTML = para
+
+}
+function hidePara(){
+    para.innerHTML =''
+}
