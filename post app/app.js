c188cecc47bd43ed9f889f2642e3da12dc45df23
ansharahamin <insharahamin1250@gmail.com>
2025-11-01 16:49:06 +0500

    post button

--- post app/app.js
+++ post app/app.js
@@ -2,21 +2,28 @@ var postInput = document.getElementById("postBox")
 var form = document.getElementById("form")
 var postCard = document.getElementById("postCard")
 var insertname = document.getElementById("insertname")
+var insertTitle = document.getElementById("insertTitle");
+var insertDescription = document.getElementById("insertDescription")
+
 // var name = document.getElementById("names")
 // var title = document.getElementById("title")
 function hideForm() {
     form.style.display = "none"
     postInput.classList.remove("d-none")
     postInput.classList.add("postBox")
-    
+
 }
 function post() {
     postCard.classList.remove("d-none")
-var name = document.getElementById("userName").value;
-var title = document.getElementById("title");
+    var name = document.getElementById("userName").value;
+    var title = document.getElementById("title").value;
+    var description = document.getElementById("Description").value;
 
-console.log("Name:", userName);
-console.log("Title:", title);
+    console.log("Name:", name);
+    console.log("Title:", title);
+    insertname.innerHTML = name;
+    insertTitle.innerHTML = title;
+    insertDescription.innerHTML = description;
     // title.innerHTML = title
     // insertname.innerHTML = name
 }
