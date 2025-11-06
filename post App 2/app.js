d3d2157f2998df3020fb91b161ef0ca76ec91f30
ansharahamin <insharahamin1250@gmail.com>
2025-11-06 18:07:00 +0500

    edit And Delete remained

--- post App 2/app.js
+++ post App 2/app.js
@@ -1,35 +1,44 @@
+var cardImg;
+
 function post() {
-  var title = document.getElementById("title").value
-  var description = document.getElementById("description").value
-  var posts = document.getElementById("posts")
+  var title = document.getElementById("title").value;
+  var description = document.getElementById("description").value;
+  var posts = document.getElementById("posts");
   console.log(title, description);
-  var imgs = ["/images/abstract-pui50-light-background-wallpaper-colorful-gradient-blurry-soft-smooth-motion-bright-shine_792836-133030.jpg", "./images/artificial-intelligence-circuit-electric-line-600nw-2495924357.jpg", "./images/gradient-background-blue-cyan-2-vector.jpg", "/images/pexels-codioful-6985001.jpg"]
-   var randomImg = imgs[Math.floor(Math.random() * imgs.length)];
+
   if (title.trim() && description.trim()) {
-    posts.innerHTML += `   <div class="col-8">
-        <div class="card text-bg-light postCard">
-          <img height="200px"
-            src="${randomImg}"
-            class="card-img" alt="...">
+    posts.innerHTML += `   <div   class="col-8">
+        <div style="background: url('${cardImg}');" class="card text-bg-light postCard">
+        
           <div class="card-img-overlay">
-            <h5 class="card-title">${title}</h5>
-            <p class="card-text">${description}</p>
-            <p class="card-text"><small>Last updated 3 mins ago</small></p>
+            <h5 class="card-title text-white">${title}</h5>
+            <p class="card-text text-white">${description}</p>
+            <p class="card-text text-white"><small>Last updated 3 mins ago</small></p>
             <div class="d-grid gap-2 d-md-flex justify-content-md-end">
               <button class="btn btn-success me-md-2" type="button">Edit</button>
               <button class="btn btn-danger" type="button">Delete</button>
             </div>
           </div>
         </div>
-      </div>`
-    document.getElementById("title").value = ""
-    document.getElementById("description").value = ""
+      </div>`;
+    document.getElementById("title").value = "";
+    document.getElementById("description").value = "";
   } else {
     Swal.fire({
       icon: "error",
       title: "Empty Post is not Allowed",
-      text: "Enter the title and description"
+      text: "Enter the title and description",
     });
   }
+}
+function clickAbleImg(src) {
+  var bgImg = document.getElementsByClassName("bgImg");
+  console.log(bgImg);
 
+  for (let i = 0; i < bgImg.length; i++) {
+    bgImg[i].classList.remove("selectedImg");
+  }
+  console.log(event.target);
+  event.target.classList.add("selectedImg");
+  cardImg = event.target.src
 }
