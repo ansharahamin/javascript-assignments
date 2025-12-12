f2e86ad02b791fa757c549d564ea408cf1e3f0cd
ansharahamin <insharahamin1250@gmail.com>
2025-12-12 18:02:40 +0500

    spread operator

---
+++ spread operator/app.js
@@ -0,0 +1,10 @@
+var arr = [1, 2, 3, 4, 5];
+function sum(a, b, c, d, e) {
+  console.log(a+ b+c+ d+e);
+}
+sum(...arr);
+var arr = pr [1, 2, 3, 4, 5];
+function sum(a, b, c, d, e) {
+  console.log(a+ b+c+ d+e);
+}
+sum(...arr);
